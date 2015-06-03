1c102af95fe9e712e850f7d5e1043add3b8a57e2
Daniel Michelson <dbm@baltrad.eu>
2015-06-03 18:29:41 +0200

    Added the functionality after this project was created

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rave_hexquant
 Conversions between ODIM quantity strings and hexed representations of the same.
+
+This simple functionality is used to exemplify unit testing and also social coding on GitHub.
---
+++ test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+'''
+Copyright (C) 2015- Swedish Meteorological and Hydrological Institute (SMHI)
+
+This file is part of RAVE, sort-of ...
+
+RAVE is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RAVE is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with RAVE.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+## Simple example unit testing framework
+
+## @file
+## @author Daniel Michelson, SMHI
+## @date 2015-06-03
+
+import unittest
+import rave_hexquant
+
+class TestHex(unittest.TestCase):
+    """
+    Our basic test class
+    """
+
+    def test_q2hex(self):
+        """
+        Test q2hex
+        Any method which starts with ``test_`` will considered as a test case.
+        """
+        result = rave_hexquant.q2hex(["TH","DBZH"])
+        self.assertEqual(result, '0x3')
+
+
+if __name__ == '__main__':
+    unittest.main()
